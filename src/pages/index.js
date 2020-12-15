fa9180327f4c5c13d7e615a81477a6699a7bd880
Elaine Williams <Elaine@nojoystick.fritz.box>
2020-12-15 03:34:14 +0100

    remove blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
-import Blog from './blog/blog';
 import Home from './home/home';
 import Music from './music/music';
 import Projects from './projects/projects';
 
-export { Blog, Home, Music, Projects };
+export { Home, Music, Projects };
